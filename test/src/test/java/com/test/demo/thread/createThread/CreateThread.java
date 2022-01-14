a533010dbcaba4ba8ea53184ae3cb12e87f43c3c
zsy <18716946062@163.com>
2022-01-14 11:09:39 +0800

    fix: thread update

--- test/src/test/java/com/test/demo/thread/createThread/CreateThread.java
+++ test/src/test/java/com/test/demo/thread/createThread/CreateThread.java
@@ -23,10 +23,10 @@ public class CreateThread {
 
     @Test
     public void ttt() throws ExecutionException, InterruptedException {
-        extendThread();
-        implementRunnable();
+        //extendThread();
+        //implementRunnable();
         implementRunnable_result();
-        implementCallable();
+        //implementCallable();
     }
 
     // 1）继承Thread类创建线程
@@ -41,40 +41,56 @@ public class CreateThread {
 
     // 2)实现Runnable接口
     public void implementRunnable() throws InterruptedException {
-        Runnable runnable = new Runnable() {
+        /*Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 log.info(">>>>>>implementRunnable");
             }
         };
         Thread.sleep(5000);
-        new Thread(runnable, "implementRunnable").start();
+        new Thread(runnable, "implementRunnable").start();*/
+        new Thread(() -> log.info(">>>>>>implementRunnable"), "implementRunnable")
+                .start();
     }
 
     // 3)实现Runnable接口,带返回值
     public void implementRunnable_result() throws ExecutionException, InterruptedException {
-        Runnable runnable = new Runnable() {
+        /*Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 log.info(">>>>>>implementRunnable_result");
             }
         };
-        FutureTask futureTask = new FutureTask(runnable, "implementRunnable_resulttttttttttt");
+        FutureTask futureTask = new FutureTask(runnable, "implementRunnable_resulttttttttttt");*/
+        FutureTask futureTask = new FutureTask(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info(">>>>>>implementRunnable_result");
+        },"implementRunnable_resulttttttttttt");
         new Thread(futureTask, "futureTask").start();
-        log.info("implementRunnable_result>>>>>>futureTask.get() {}", futureTask.get());
+        log.info("implementRunnable_result(result): {}", futureTask.get());
     }
 
     // 4)带返回值的线程(实现implements  Callable<返回值类型>)
     public void implementCallable () throws ExecutionException, InterruptedException {
-        //FutureTask futureTask = new FutureTask(callable);
-        FutureTask futureTask = new FutureTask(() -> {
+       /* FutureTask futureTask = new FutureTask(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                log.info(">>>>>>implementCallable");
+                return "Callableeeeeeeeeeee";
+            }
+        });*/
+        FutureTask<String> futureTask = new FutureTask(() -> {
             log.info(">>>>>>implementCallable");
+            Thread.sleep(6000);
             return "Callableeeeeeeeeeee";
         });
-        Thread.sleep(10000);
         Thread thread = new Thread(futureTask, "implementCallable");
         thread.start();
-        log.info("implementCallable>>>>>>futureTask.get() {}", futureTask.get());
+        log.info("implementCallable(result): {}", futureTask.get());
     }
 
 
--- test/src/test/java/com/test/mytest/MyTest.java
+++ test/src/test/java/com/test/mytest/MyTest.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.test.entity.User;
 import com.test.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.FutureTask;
 
 /**
  * @author: ZSY
@@ -29,8 +33,36 @@ public class MyTest {
 
     @Test
     public void test(){
-        userService.list().forEach(System.out::println);
+        long start = System.currentTimeMillis();
+        //userService.list().forEach(System.out::println);
+        userService.list();
+        long end = System.currentTimeMillis();
+        log.info("耗时： {}秒", (end - start)/1000);
     }
 
+    @Test
+    public void threadUserTest() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            List<User> list = userService.list();
+        }
+        long end = System.currentTimeMillis();
+        log.info("耗时： {}秒", (end - start)/1000);
+
+    }
+
+    @Test
+    public void threadUserTest2() {
+        long start = System.currentTimeMillis();
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            List<User> list = userService.list();
+            return "success";
+        });
+        for (int i = 0; i < 10000; i++) {
+            new Thread(futureTask).start();
+        }
+        long end = System.currentTimeMillis();
+        log.info("耗时： {}秒", (end - start)/1000);
+    }
 
 }
